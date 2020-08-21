649e5a2438f1a18141ff44401cfe3476ca1f9f54
Julien Xemard <julien.xemard@outlook.com>
2020-08-21 20:18:36 +0200

    Linked BE & FE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import axios from 'axios'
 
-const App = () => {
-  return (
-    <h1>Hello World</h1>
-  )
+class App extends React.Component {
+  async componentDidMount() {
+    try {
+      const res = await axios.get('/api/surfspots')
+      console.log(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  } 
+
+render() {
+  return <h1>Nalu here we are</h1>
+  }
 }
+
 
 export default App
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 app.use(logger)
 
-app.use(router)
+app.use('/api', router)
 
 app.use(errorHandler)
 
